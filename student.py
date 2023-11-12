4bccdaccd70227a4dd01a9d8ec17db9a781d2efe
kelvinokumba <okumbakelvin@gmail.com>
2023-11-12 07:08:58 +0300

    sql

--- student.py
+++ student.py
@@ -16,5 +16,52 @@ class Student(Base):
     age=Column(Integer)
     grade=Column(Integer)
 
-Base.metadata.create_engine(engine)
+# Base.metadata.create_engine(engine)
+
+student1= Student(name='kelly', age=24, grade='Fifth')
+student23= Student(name='Wanyungu', age='29', grade='First')
+
+session.add(student1)
+#session.add_all(student1,student2)
+session.commit()
+
+#Get all data
+students = session.querry(Student)
+for student in students:
+    print(student.name,student.age,student.age)
+
+#get data by order
+students =session.query(Student).order_by(student.name)
+
+for student in students:
+    print(student.name)
+    
+#get data by filtering
+student =session.query(student).filter(student.name=="jerrin",).first()
+
+for student in students:
+    print(student.name)
+    
+student = session.query(Student).filter(student.name=="jane", student.name=="kellly")
+
+for student in students:
+    print(student.name, student.age)
+
+#count of results
+student_count = session.query(Student).filter(or(student.name=="jane", student.name="kellly")).count()
+print(student_count)
+ 
+ 
+# update data
+student = session.query(Student).filter(Student.name=='kelvin').first()
+student.name="kevin"
+session.commit()
+print(student.name)
+
+
+#delete data
+student =session.qury(Student).filter(Student.name=="kelvin").first()
+session.delete(student)
+session.commit()
+print(student.name)
 
